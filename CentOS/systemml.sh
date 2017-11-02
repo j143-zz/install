cb14bb548467bb5391a08725a6998d6c02533cd4
j143 <j143@protonmail.com>
2017-11-02 17:05:22 +0530

    Update systemml.sh

--- CentOS/systemml.sh
+++ CentOS/systemml.sh
@@ -5,5 +5,9 @@ cd systemml
 
 mvn clean package
 
+# set spark path
+export SPARK_HOME=$HOME/spark-2.2.0-bin-hadoop2.7
+export PATH=$PATH:$SPARK_HOME/bin
+
 # for running a specific script
 spark-submit target/SystemML.jar -f scripts/nn/test/run_tests.dml
