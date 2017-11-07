1180c3c1eedb7030e0a3f76cf5838477cd270470
j143 <j143@protonmail.com>
2017-11-07 21:53:12 +0530

    Update systemml.sh

--- CentOS/systemml.sh
+++ CentOS/systemml.sh
@@ -11,3 +11,9 @@ export PATH=$PATH:$SPARK_HOME/bin
 
 # for running a specific script
 spark-submit target/SystemML.jar -f scripts/nn/test/run_tests.dml
+
+# for running mnist example
+scripts/nn/examples/get_mnist_data.sh
+
+spark-submit target/SystemML.jar -f scripts/nn/examples/mnist_softmax-train.dml -nvargs train=scripts/nn/examples/data/mnist/mnist_train.csv test=scripts/nn/ex
+amples/data/mnist/mnist_test.csv epochs=1 
