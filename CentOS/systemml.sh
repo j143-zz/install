c1d54c31ae609bdbd78c452802c880b453d67fd9
j143 <j143@protonmail.com>
2017-11-07 22:05:37 +0530

    Update systemml.sh

--- CentOS/systemml.sh
+++ CentOS/systemml.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # commands for running systemml
+su -
 
 cd systemml
 
@@ -12,8 +13,12 @@ export PATH=$PATH:$SPARK_HOME/bin
 # for running a specific script
 spark-submit target/SystemML.jar -f scripts/nn/test/run_tests.dml
 
-# for running mnist example
+# for running mnist example 
 scripts/nn/examples/get_mnist_data.sh
 
-spark-submit target/SystemML.jar -f scripts/nn/examples/mnist_softmax-train.dml -nvargs train=scripts/nn/examples/data/mnist/mnist_train.csv test=scripts/nn/ex
-amples/data/mnist/mnist_test.csv epochs=1 
+## 1. mnist softmax testing
+spark-submit target/SystemML.jar -f scripts/nn/examples/mnist_softmax-train.dml -nvargs train=scripts/nn/examples/data/mnist/mnist_train.csv test=scripts/nn/examples/data/mnist/mnist_test.csv epochs=1 out_dir=scripts/nn/examples/model/mnist_softmax
+
+## 2. mnist lenet
+spark-submit target/SystemML.jar -f scripts/nn/examples/mnist_lenet-train.dml -nvargs train=scripts/nn/examples/data/mnist/mnist_train.csv test=scripts/nn/examples/data/mnist/mnist_test.csv C=1 Hin=28 Win=28 epochs=10 out_dir=scripts/nn/examples/model/mnist_lenet
+
