5dda86d150f8ce0d4cca18557d460fed3d6bb4d3
j143 <j143@protonmail.com>
2017-11-17 22:37:14 +0530

    Create Multimedia.sh

---
+++ CentOS/Multimedia.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# install Adobe pdf reader
+
+$ su -
+Password:
+# wget http://ardownload.adobe.com/pub/adobe/reader/unix/9.x/9.5.5/enu/AdbeRdr9.5.5-1_i486linux_enu.rpm
+# exit
+$ acroread
